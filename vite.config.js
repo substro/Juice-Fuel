5a844f950543d033dd96c249b4f46e6163c78a16
Ali Amin <prof.aliamin@gmail.com>
2023-06-01 17:15:42 +0300

    import bug fix

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+	build: {
+    rollupOptions: {
+      external: [
+        // other external modules
+        "react-icons/fa"
+      ]
+    }
+  }
+
 })
