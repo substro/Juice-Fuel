32ddc39108d5474306291e9b946da1ffb2416b65
Ali Amin <prof.aliamin@gmail.com>
2023-06-01 17:03:07 +0300

    removing unused imports

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,5 @@
 import React from "react"
-import { useState, useEffect } from "react"
 import { Nav } from "./Nav"
-import { FaBars } from "react-icons/fa"
 
 export const Header = () => {
   return (
