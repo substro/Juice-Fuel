9c4485809f6d622714417b1628e010fc88b952e8
Ali Amin <prof.aliamin@gmail.com>
2023-06-01 16:57:28 +0300

    main section preparation

--- src/MainSection.jsx
+++ src/MainSection.jsx
@@ -3,7 +3,16 @@ import React from "react"
 export const MainSection = () => {
   return (
     <>
-      <div></div>
+      <div className="menu-title">
+        <div className="ball"></div>
+        <div className="ball"></div>
+        <div className="ball"></div>
+        <h2>Menu</h2>
+        <div className="ball"></div>
+        <div className="ball"></div>
+        <div className="ball"></div>
+      </div>
+      <div className="main-section"></div>
     </>
   )
 }
