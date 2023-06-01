03acb46a7a2e3e683440b991babfd58a21916056
Ali Amin <prof.aliamin@gmail.com>
2023-06-01 17:53:12 +0300

    mistake

---
+++ src/Header.jsx
@@ -0,0 +1,13 @@
+import { NavBar } from "./NavBar"
+import logo from "./assets/images/logo.png"
+export const Header = () => {
+  return (
+    <header className="header">
+      <a href="#">
+        <img src={logo} alt="logo" className="logo" />
+      </a>
+
+      <NavBar />
+    </header>
+  )
+}
