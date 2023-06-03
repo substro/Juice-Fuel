377144e349fc762b86503a9e9ea4b917f3bf883b
Ali Amin <prof.aliamin@gmail.com>
2023-06-03 20:28:42 +0300

    added fruits

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import { Footer } from "./Footer"
-import { MainSection } from "./MainSection"
-import { Header } from "./Header"
+import { Footer } from "./Footer";
+import { MainSection } from "./MainSection";
+import { Header } from "./Header";
+import { Fruits } from "./Fruits";
 
 function App() {
   return (
     <>
+      <Fruits />
       <Header />
       <MainSection />
       <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Fruits.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import leftFruits from "./assets/images/fruits-left.png";
+import rightFruits from "./assets/images/fruits-right.png";
+
+export const Fruits = () => {
+  return (
+    <div className="fruits-container">
+      <div className="left">
+        <img className="fruit-image" src={leftFruits} alt="fruits" />
+      </div>
+      <div className="right">
+        <img className="fruit-image" src={rightFruits} alt="fruits" />
+      </div>
+    </div>
+  );
+};
