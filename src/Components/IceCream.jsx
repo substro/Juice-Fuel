cf5f6033ee0d7f21df7eac1aac99fd6b800a156a
Ali Amin <prof.aliamin@gmail.com>
2023-06-05 12:51:01 +0300

    iceCream #1

--- src/Components/IceCream.jsx
+++ src/Components/IceCream.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { GiIceCreamCone } from "react-icons/gi";
+import {hot} from "../Data/Drinks"
 
 export const IceCream = () => {
   return (
@@ -11,7 +12,46 @@ export const IceCream = () => {
         <h3>Ice Cream</h3>
       </div>
       <div className="ice-cream-container">
-        
+        <table>
+          <thead>
+            <tr className="item size">
+              <td>
+                <h1></h1>
+              </td>
+              <td className="sizes">
+                <span>Small</span>
+                <span>Medium</span>
+                <span>Large</span>
+                <span>Bottle</span>
+              </td>
+              <td>
+                <h1></h1>
+              </td>
+            </tr>
+          </thead>
+
+          <tbody>
+            {hot.map((x) => {
+              return (
+                <tr className="item" key={x.id}>
+                  <td>
+                    <h1>{x.name.en}</h1>
+                  </td>
+
+                  <td className="prices">
+                    {x.sizes.map((y, index) => {
+                      return <span key={index}>{y.price}</span>;
+                    })}
+                  </td>
+
+                  <td>
+                    <h1>{x.name.ar}</h1>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
       </div>
     </>
   );
