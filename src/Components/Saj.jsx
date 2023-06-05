10cc4f355b561fbed3aa23a24f9d279a4b58c5df
Ali Amin <prof.aliamin@gmail.com>
2023-06-05 23:11:31 +0300

    saj #1

--- src/Components/Saj.jsx
+++ src/Components/Saj.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { GiFullPizza } from "react-icons/gi";
 import { schedules } from "../Data/Schedule";
+import { saj } from "../Data/Saj";
 
 export const Saj = () => {
   return (
@@ -13,6 +14,33 @@ export const Saj = () => {
         <div className="ball"></div>
         <h4>{schedules.saj}</h4>
       </div>
+      <saj-container>
+        <table>
+          <thead>
+            <tr className="item size">
+              <td>
+                <h1></h1>
+              </td>
+              <td>
+                <h1>مناقيش</h1>
+              </td>
+            </tr>
+          </thead>
+
+          <tbody>
+            {saj.map((x) => {
+              return (
+                <tr className="item" key={x.id}>
+                  <td className="prices">{x.price}</td>
+                  <td>
+                    <h1>{x.name}</h1>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </saj-container>
     </>
   );
 };
