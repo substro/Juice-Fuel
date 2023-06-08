d7b9debefa247a9a9f13c6fe2830e713a3040f9c
Ali Amin <prof.aliamin@gmail.com>
2023-06-08 12:43:34 +0300

    drinks hot #1

--- src/Components/Hot.jsx
+++ src/Components/Hot.jsx
@@ -1,58 +1,56 @@
-import React from "react";
-import { LuCoffee } from "react-icons/lu";
-import { hot } from "../Data/Drinks";
+import React from 'react'
+import { LuCoffee } from 'react-icons/lu'
+import { hot } from '../Data/Drinks'
 
 export const Hot = () => {
-  return (
-    <div className="drink-container-hot">
-      <div className="section-title">
-        <div className="icon">
-          <LuCoffee />
-        </div>
-        <h3>Drinks</h3>
-        <div className="ball"></div>
-        <h4>Hot</h4>
-      </div>
-      <table>
-        <thead>
-          <tr className="item size">
-            <td>
-              <h1></h1>
-            </td>
-            <td className="sizes">
-              <span>Small</span>
-              <span>Medium</span>
-              <span>Large</span>
-              <span>Bottle</span>
-            </td>
-            <td>
-              <h1></h1>
-            </td>
-          </tr>
-        </thead>
+	return (
+		<div className='drink-container-hot'>
+			<div className='section-title'>
+				<div className='icon'>
+					<LuCoffee />
+				</div>
+				<h3>Drinks</h3>
+				<div className='ball'></div>
+				<h4>Hot</h4>
+			</div>
+			<table>
+				<thead>
+					<tr className='item size'>
+						<td>
+							<h1></h1>
+						</td>
+						<td className='sizes'>
+							<span>Small</span>
+							<span>Medium</span>
+						</td>
+						<td>
+							<h1></h1>
+						</td>
+					</tr>
+				</thead>
 
-        <tbody>
-          {hot.map((x) => {
-            return (
-              <tr className="item" key={x.id}>
-                <td>
-                  <h1>{x.name.en}</h1>
-                </td>
+				<tbody>
+					{hot.map((x) => {
+						return (
+							<tr className='item' key={x.id}>
+								<td>
+									<h1>{x.name.en}</h1>
+								</td>
 
-                <td className="prices">
-                  {x.sizes.map((y, index) => {
-                    return <span key={index}>{y.price}</span>;
-                  })}
-                </td>
+								<td className='prices'>
+									{x.sizes.map((y, index) => {
+										return <span key={index}>{y.price}</span>
+									})}
+								</td>
 
-                <td>
-                  <h1>{x.name.ar}</h1>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </div>
-  );
-};
+								<td>
+									<h1>{x.name.ar}</h1>
+								</td>
+							</tr>
+						)
+					})}
+				</tbody>
+			</table>
+		</div>
+	)
+}
