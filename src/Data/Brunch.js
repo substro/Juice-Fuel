aad33064eec59de1bd84792d373621f8f9e3d64b
Ali Amin <112518638+substro@users.noreply.github.com>
2024-12-09 20:35:59 +0200

    Update Brunch.js

--- src/Data/Brunch.js
+++ src/Data/Brunch.js
@@ -1,49 +1,58 @@
 export const addOns = [
 	{
 		name: "Brownie",
-		
+		price: "",
 	},
 	{
 		name: "Crunch",
-		
+		price: "",
 	},
 	{
 		name: "Milka",
-		
+		price: "",
 	},
 	{
 		name: "Peanut Butter",
-			},
+		price: "",
+	},
 	{
 		name: "Oreo",
-			},
+		price: "",
+	},
 	{
 		name: "Galaxy",
-		
+		price: "",
 	},
 	{
 		name: "Kitkat",
-		
+		price: "",
 	},
 	{
 		name: "Mozzarella",
-			},
+		price: "",
+	},
 	{
 		name: "Lotus Biscuit",
-			},
+		price: "",
+	},
 	{
 		name: "Digestive",
-			},
+		price: "",
+	},
 	{
 		name: "Strawberry",
-			},
+		price: "",
+	},
 	{
 		name: "Kiwi",
-			},
+		price: "",
+	},
 	{
-		name: "Banana",	
+		name: "Banana",
+		price: "",
 	},
 	{
 		name: "Twix",
+		price: "",
 	},
 ];
