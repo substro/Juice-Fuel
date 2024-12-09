421dae7606613c66ed3997418736c5644aa18289
Ali Amin <112518638+substro@users.noreply.github.com>
2024-12-09 20:13:05 +0200

    Update Brunch.js

--- src/Data/Brunch.js
+++ src/Data/Brunch.js
@@ -1,145 +1,54 @@
 export const addOns = [
 	{
 		name: "Brownie",
-		price: "0.75$",
+		
 	},
 	{
 		name: "Crunch",
-		price: "0.75$",
+		
 	},
 	{
 		name: "Milka",
-		price: "0.75$",
+		
 	},
 	{
 		name: "Peanut Butter",
-		price: "0.5$",
-	},
+			},
 	{
 		name: "Oreo",
-		price: "0.5$",
-	},
+			},
 	{
 		name: "Galaxy",
-		price: "0.75$",
+		
 	},
 	{
 		name: "Kitkat",
-		price: "0.75$",
+		
 	},
 	{
 		name: "Mozzarella",
-		price: "1.0$",
-	},
+			},
 	{
 		name: "Lotus Biscuit",
-		price: "0.5$",
-	},
+			},
 	{
 		name: "Digestive",
-		price: "0.5$",
-	},
+			},
 	{
 		name: "Strawberry",
-		price: "0.5$",
-	},
+			},
 	{
 		name: "Kiwi",
-		price: "0.5$",
-	},
+			},
 	{
 		name: "Banana",
-		price: "0.25$",
+		
 	},
 	{
 		name: "Twix",
-		price: "1.0$",
-	},
-];
-
-export const crepes = [
+			},
 	{
-		name: "Nutella",
-		price: "4.0$",
-		extra: "1.0$",
-	},
-	{
-		name: "Kinder",
-		price: "4.0$",
-		extra: "1.0$",
-	},
-	{
-		name: "Lotus",
-		price: "4.0$",
-		extra: "1.0$",
-	},
-	{
-		name: "Fettuccine",
-		price: "5.0$",
-	},
-	{
-		name: "Dark/White chocolate",
-		price: "4.0$",
-		extra: "1.0$",
-	},
-	{
-		name: "Pistachio Mix",
-		price: "5.0$",
-		extra: "1.0$",
-	},
-	{
-		name: "Special Combo Crépe",
-		price: "7.0$",
-		extra: "1.0$",
-	},
-	{
-		name: "Juice Fuel Crepe Roll",
-		price: "8.0$",
-	},
-];
-
-export const waffles = [
-	{
-		name: "Chocolate & Fruits",
-		price: "5.0$",
-	},
-];
-export const qashta = [
-	{
-		name: "Qashta pltate Medium",
-		price: "$10.00",
-	},
-	{
-
-		name: "Qashta Plate Large",
-		price: "$15.00",
-	
-	},
-	{
-
-		name: "Brownie Plate",
-		price: "$7.00",
-	
-	},
-	
-];
-
-export const cups = [
-	{
-		name: "Brownie",
-		price: "$4.00",
-	},
-	{
-
-		name: "Pistachio",
-		price: "$4.00",
-	
-	},
-	{
-
-		name: "Qashta",
-		price: "$4.00",
-	
-	},
-	
+		name: "",
+    price: "1.0$"
+			},
 ];
