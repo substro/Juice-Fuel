733d8a550523b1e653412243aead56b3dc45bc1a
Ali Amin <112518638+substro@users.noreply.github.com>
2024-12-09 18:45:35 +0200

    Update Saj.js

--- src/Data/Saj.js
+++ src/Data/Saj.js
@@ -7,42 +7,42 @@ export const saj = [
 	{
 		id: 2,
 		name: "زعتر",
-		price: "0.5$",
+		price: "0.75$",
 	},
 	{
 		id: 3,
 		name: "جبنه",
-		price: "1.25$",
+		price: "1.5$",
 	},
 	{
 		id: 4,
 		name: "جبنه و زعتر",
-		price: "1.25$",
+		price: "1.5$",
 	},
 	{
 		id: 5,
 		name: "جبنه و مرتديلا",
-		price: "1.75$",
+		price: "2.0$",
 	},
 	{
 		id: 6,
 		name: "جبنه و حبش",
-		price: "1.75$",
+		price: "2.0$",
 	},
 	{
 		id: 7,
 		name: "جبنة و سجق",
-		price: "2.0$",
+		price: "2.5$",
 	},
 	{
 		id: 8,
 		name: "٣ انواع جبنة",
-		price: "2.0$",
+		price: "2.5$",
 	},
 	{
 		id: 9,
 		name: "جبنة و حبش مدخن",
-		price: "2.0$",
+		price: "2.5$",
 	},
 	{
 		id: 9,
@@ -52,22 +52,22 @@ export const saj = [
 	{
 		id: 10,
 		name: "كشك",
-		price: "1.0$",
+		price: "1.5$",
 	},
 	{
 		id: 11,
 		name: "كشك و جبنة",
-		price: "1.5$",
+		price: "2.0$",
 	},
 	{
 		id: 12,
 		name: "لبنة",
-		price: "0.65$",
+		price: "1$",
 	},
 	{
 		id: 13,
 		name: "لبنة مع خضرة",
-		price: "1.0$",
+		price: "1.5$",
 	},
 	{
 		id: 14,
@@ -87,14 +87,14 @@ export const saj = [
 	{
 		id: 17,
 		name: " ميكس صاج",
-		price: "3.0$",
+		price: "3.5$",
 	},
 ];
 export const extra = [
 	{
 		id: 1,
 		name: "صحن لبنة",
-		price: "1.5$",
+		price: "2$",
 	},
 	{
 		id: 2,
@@ -111,43 +111,43 @@ export const sweets = [
 	{
 		id: 2,
 		name: "بوك و عسل",
-		price: "2.5$",
+		price: "3.0$",
 	},
 	{
 		id: 3,
 		name: "نوتيلا",
-		price: "2.5$",
+		price: "3.0$",
 	},
 	{
 		id: 4,
 		name: "نوتيلا، لوتس، موز",
-		price: "3.5$",
+		price: "4.0$",
 	},
 	{
 		id: 5,
 		name: "قشطة و عسل",
-		price: "2.5$",
+		price: "3.0$",
 	},
 ];
 export const drinks = [
 	{
 		id: 1,
 		name: "Pepsi",
-		price: "0.70$",
+		price: "1$",
 	},
 	{
 		id: 2,
 		name: "Miranda",
-		price: "0.70$",
+		price: "1$",
 	},
 	{
 		id: 3,
 		name: "7-Up",
-		price: "0.70$",
+		price: "1$",
 	},
 	{
 		id: 4,
 		name: "Juice",
-		price: "0.25$",
+		price: "0.5$",
 	},
 ];
