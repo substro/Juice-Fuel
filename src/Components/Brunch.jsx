4813d1a5a01f94c0a7a9d12b235987aa535d0d86
Ali Amin <112518638+substro@users.noreply.github.com>
2024-12-11 11:10:41 +0200

    Update Brunch.jsx

--- src/Components/Brunch.jsx
+++ src/Components/Brunch.jsx
@@ -1,33 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { MdBrunchDining } from "react-icons/md";
-import { addOns, crepes, qashta, waffles } from "../Data/Brunch";
+import { addOns, crepes, qashta, waffles, cups } from "../Data/Brunch";
 
 export const Brunch = () => {
-	// const [Brunch, setAllMenu] = useState();
-
-	// useEffect(() => {
-	// 	const getBruch = async () => {
-	// 		try {
-	// 			const response = await fetch(
-	// 				'https://script.google.com/macros/s/AKfycbwQvjoMdolN0LvAXXEdJfFES9HhCathgpckLDEZ-zI39zbuHp0TOyDNYcB9eiRnTkGg/exec'
-	// 			);
-
-	// 			if (!response.ok) {
-	// 				throw new Error('Network response was not ok');
-	// 			}
-
-	// 			const {data} = await response.json();
-	// 			setAllMenu(data);
-	// 			console.log("🚀 ~ file: Brunch.jsx:21 ~ getBruch ~ data:", data)
-	// 			// Do something with the data here
-	// 		} catch (error) {
-	// 			console.error('Error fetching data:', error);
-	// 		}
-	// 	};
-
-	// 	getBruch();
-	// }, []); // Empty array means this effect runs once on component mount
-
 	return (
 		<>
 			<div className="section-title">
@@ -59,9 +34,19 @@ export const Brunch = () => {
 					</ul>
 				</div>{" "}
 				<div className="brunch">
-					<h4>قشطة</h4>
+					<h4>Qashta</h4>
 					<ul className="items">
 						{qashta.map((item, index) => (
+							<li key={index}>
+								<span className="name">{item.name}</span> : {item.price}{" "}
+							</li>
+						))}
+					</ul>
+				</div>
+				<div className="brunch">
+					<h4>Cups</h4>
+					<ul className="items">
+						{cups.map((item, index) => (
 							<li key={index}>
 								<span className="name">{item.name}</span> : {item.price}{" "}
 							</li>
